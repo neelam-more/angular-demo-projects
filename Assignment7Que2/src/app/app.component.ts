2937fa9ac0c7060a1a68010175be502f453fb4a6
neelam-more <neelammore20@gmail.com>
2024-01-15 20:16:28 -0500

    Added Assignment7 Que2

---
+++ Assignment7Que2/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment7Que2';
+
+  public fun():any
+  {
+
+    return "Marvellous Infosystems.";
+    
+  }
+}
